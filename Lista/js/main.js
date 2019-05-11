473cf7659b8d77faedcc1c6d49a3503b4083623a
AndrePereiraSF <andre-filho08@hotmail.com>
2019-05-11 16:09:01 -0300

    Comment Lista/js/main.js

--- Lista/js/main.js
+++ Lista/js/main.js
@@ -1,48 +1,52 @@
-var list = new LinkedList();
+var list = new LinkedList(); // a variável list recebe uma nova instância de LinkedList
 
-$().ready( () => {
+$().ready( () => { // adiciona functions aos botões no HTML via id
     $('#insert').click(insertElement)
     $('#insert_at').click(insertElementAt)
     $('#remove').click(removeElement)
     $('#remove_at').click(removeElementAt)
 });
 
-function showData(){
+function showData(){ // lista os elementos da lista dentro de divs com a classe ui label
     let text = `<div class="ui label">
                     ${list.show('</div><div class="ui label">')}
-                </div>`
+                </div>` // texto é "setado"
     let out = $('#output');
-    out.empty()
-    out.append(text)
+    out.empty() // div com id output é esvaziada
+    out.append(text) // text é adicionado dentro da div com id output
 }
-function insertElement(){
-    let val = parseInt(prompt('digite um valor a ser inserido:'));
-    if (list.search(val)){
+
+function insertElement(){ // insere elementos na lista
+    let val = parseInt(prompt('digite um valor a ser inserido:')); // recebe o elemento
+    if (list.search(val)){ // verifica se o elemento já existe na lista e impede que este seja adicionado
          alert("valor já inserido")
-    }else{
-        list.append(val);
+        }else{ // adiciona o valor na lista e mostra a lista atualizada
+            list.append(val); 
         showData();
     }
 }
-function insertElementAt(){
-    let val = parseInt(prompt('digite um valor a ser inserido:'));
-    if (list.search(val)){
+
+function insertElementAt(){ // insere um elemento em determinada posição
+    let val = parseInt(prompt('digite um valor a ser inserido:')); // recebe o elemento
+    if (list.search(val)){ // verifica se ele já existe na lista e impede que este seja adicionado
         alert("valor já inserido")
    }else{
-        let pos = parseInt(prompt('digite uma posição a inserir:'));
-        list.insert(pos, val);
-        showData();
+        let pos = parseInt(prompt('digite uma posição a inserir:')); // recebe o index
+        list.insert(pos, val); // insere o elemento na lista no index recebido
+        showData(); // mostra a lista atualizada
    }
 }
-function removeElement(){
-    let val = parseInt(prompt('digite um valor a ser removido:'));
-    let rm = list.remove(val);
+
+function removeElement(){ // remove um elemento da lista
+    let val = parseInt(prompt('digite um valor a ser removido:')); // recebe o elemento a ser removido
+    let rm = list.remove(val); // remove o elemento da lista
     if (rm) alert(`Foi removido o valor ${rm}`)
-    showData();
+    showData(); // mostra lista atualizada
 }
-function removeElementAt(){
-    let pos = parseInt(prompt('digite uma posição a remover:'));
-    let rm = list.removeAt(pos)
-    if (rm) alert(`Foi removido o valor ${rm}`)
-    showData();
+
+function removeElementAt(){ // remove elemento em determinada posição
+    let pos = parseInt(prompt('digite uma posição a remover:')); // recebe a posição 
+    let rm = list.removeAt(pos) // remove elemento na posição recebida
+    if (rm) alert(`Foi removido o valor ${rm}`) // se foi possível remover, um alert é acionado
+    showData(); // mostra lista atualizada
 }
